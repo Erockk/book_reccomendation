e31440212778fd06b539440754a7b7a33e647dd6
Erik Olson <erikolson90@gmail.com>
2020-09-14 22:20:43 -0600

    pushing to repo

---
+++ book_recommendation.py
@@ -0,0 +1,39 @@
+import os
+import booklist
+
+
+class MyProj:
+    def __init__(self)
+    self.test = None
+    self.book_list = None
+
+    def run(self):
+        self.booklist = open("D:\\Glorious Projects\Python Practice\booklist")
+        for range(x, sizeof(book_list)):
+            sdf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    test = MyProj()
+    test.run()
+
